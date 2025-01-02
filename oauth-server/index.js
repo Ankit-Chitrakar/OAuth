e9d885e5e604ab9d0cf4a556a8a291dd868cb6e9
Ankit-Chitrakar <chitrakarankit2003@gmail.com>
2025-01-02 18:08:16 +0530

    Feat: Google OAuth implementation

--- oauth-server/index.js
+++ oauth-server/index.js
@@ -22,7 +22,10 @@ app.get('/auth/github/callback', async (req, res)=>{
     const { code } = req.query;
 
     try{
-        console.log('Code received from GitHub:', req.query.code);
+        if(!code){
+            return res.status(404).json({message: 'Authorization code was not provided'});
+        }
+
         const tokenResponse = await axios.post(`https://github.com/login/oauth/access_token`, {
             client_id: process.env.GITHUB_CLIENT_ID,
             client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -38,6 +41,34 @@ app.get('/auth/github/callback', async (req, res)=>{
         res.status(500).json({message: err.message})
     }
 });
+
+app.get('/auth/google', (req, res) => {
+    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:4000/auth/google/callback&response_type=code&scope=profile+email`;
+
+    res.redirect(googleAuthUrl);
+})
+
+app.get('/auth/google/callback', async (req, res) => {
+    const {code} = req.query;
+    try{
+        if(!code){
+            return res.status(404).json({message: 'Authorization code was not provided'});
+        }
+
+        const tokenResponse = await axios.post(`https://oauth2.googleapis.com/token`, {
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            client_secret: process.env.GOOGLE_CLIENT_SECRET,
+            code,
+            redirect_uri: `http://localhost:4000/auth/google/callback`,
+            grant_type: 'authorization_code'
+        }, {headers: {"Content-Type": "application/x-www-form-urlencoded"}})
+
+        res.cookie('access_token', tokenResponse.data.access_token);
+        res.redirect(`${process.env.FRONTEND_BASE_URL}/v1/profile/google`)
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
